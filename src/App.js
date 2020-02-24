71ab99871850c787c44c25c0beb698073320e261
sayantanHack <sayantandatta97@gmail.com>
2020-02-25 00:21:47 +0530

    adding BodyElement , Maps ,& the json data.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import MapContainer from './components/Map';
+import BodyElement from './components/Body';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Resturant review App</h1>
-
+        <h1 className="nav">Resturant review App</h1>
         <MapContainer />
+
+        <BodyElement />
       </div>
     );
   }
---
+++ src/components/Body.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BodyElement extends Component {
+    render() {
+        return (
+            <div className="Body">
+                <h2>The Resturants list</h2>
+            </div>
+        );
+    }
+}
+
+export default BodyElement;
