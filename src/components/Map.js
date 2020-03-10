07754782a29ccea599179eba821b7f6dca078525
sayantanHack <sayantandatta97@gmail.com>
2020-03-10 11:36:44 +0530

    Adding marker on maps.

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
-import * as restaurantData from './data/restaurant.json';
+import restaurants from './data/restaurant.json';
 
 const mapStyles = {
     width: '70%',
@@ -8,13 +8,28 @@ const mapStyles = {
 };
 
 export class MapContainer extends Component {
-
+/*
     state = {
         showingInfoWindow: false, //Hides or the shows the infoWindow
         activeMarker: {}, //Shows the active marker upon click
         selectedPlace: {} //Shows the infoWindow to the selected place upon a marker
     };
 
+    constructor(props) {
+        super(props);
+        const {lat,lng} = this.props.initialCenter
+    }
+
+    <Marker onClick={this.onMarkerClick} name={} />
+                <InfoWindow
+                    marker= {this.state.activeMarker} visible={this.state.showingInfoWindow}
+                    onClose={this.onClose}
+                >
+                    <div>
+                        <h4>{this.state.selectedPlace.name}</h4>
+                    </div>
+                </InfoWindow>
+*/
     onMarkerClick = (props, marker, e) =>
         this.setState({
             selectedPlace: props,
@@ -31,15 +46,39 @@ export class MapContainer extends Component {
         }
     }
     render() {
-        return (<
+        return (/*<
             Map google={this.props.google}
             zoom={10}
             style={mapStyles}
             initialCenter={
                 { lat: 22.6072325, lng: 88.2825886 }
-            }
-        />
+            }>
 
+                
+            </Map>*/
+            <Map google={this.props.google}
+    style={{width: '100%', height: '100%', position: 'relative'}}
+    className={'map'}
+    zoom={14}>
+  <Marker onClick={this.onMarkerClick}
+    title={'The marker`s title will appear as a tooltip.'}
+    name={'SOMA'}
+    position={{lat: 37.778519, lng: -122.405640}} />
+  <Marker onClick={this.onMarkerClick}
+    name={'Dolores park'}
+    position={{lat: 37.759703, lng: -122.428093}} />
+  
+  
+
+        <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              <h1>Hello Map</h1>
+            </div>
+        </InfoWindow>
+
+</Map>
+
+             
         );
     }
 
--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -4,6 +4,7 @@ import restaurants from './data/restaurant.json';
 class RestaurantList extends Component{
     constructor(props){
         super(props);
+        this.addReview = this.addReview.bind(this);
         this.state = {
             //let restaurants = 
             //restaurants: this.props.restaurants
@@ -19,7 +20,7 @@ class RestaurantList extends Component{
     
     addReview(restaurantId, review){
         let restaurants= this.state.restaurants;
-       let restaurant = this.restaurants.select((id) => this.id = restaurantId)
+        let restaurant = this.restaurants.select((id) => this.id = restaurantId)
         let reviews = restaurant.reviews || [];
         reviews.push(review)
         restaurants.map((res) => {
@@ -33,7 +34,10 @@ class RestaurantList extends Component{
         return(
             <div>
                 <h3>Restaurants are here. Have a look.</h3>
-                                
+                <form>
+                    <input type="text" placeholder="add your reviews"></input>
+                    <button onClick={this.addReview}>Submit</button>
+                </form>              
             </div>
         )
     }
